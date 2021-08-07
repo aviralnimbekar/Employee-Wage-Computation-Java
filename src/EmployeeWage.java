15b73dad6290e36e8831d2242393de698226add0
aviralnimbekar <aviralnimbekar12@gmail.com>
2021-08-08 01:49:22 +0530

    Solved using Switch Case Statement

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -3,22 +3,24 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
 
-        int is_Full_Time_Present = 1;
-        int is_Part_Time_Present = 2;
+        //  is_Full_Time_Present = 1;
+        //  is_Part_Time_Present = 2;
         int emp_Wage_Per_hrs = 20;
         int emp_Working_Hrs = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 3;
+        int empCheck = (int)(Math.floor(Math.random() * 10) % 3);
 
-        if ( empCheck == is_Full_Time_Present ) {
-            System.out.println("Employee is Full Time Present");
-            emp_Working_Hrs = 8;
-        }
-        else if ( empCheck == is_Part_Time_Present ) {
-            System.out.println("Employee is Part Time Present");
-            emp_Working_Hrs = 4;
-        }
-        else {
-            System.out.println("Employee is Absent");
+        switch (empCheck) {
+            case 1:
+                System.out.println("Employee is Full Time Present");
+                emp_Working_Hrs = 8;
+            break;
+            case 2:
+                System.out.println("Employee is Part Time Present");
+                emp_Working_Hrs = 4;
+            break;
+            default:
+                System.out.println("Employee is Absent");
+            break;
         }
 
         int Salary = emp_Wage_Per_hrs * emp_Working_Hrs;
