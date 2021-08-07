e3a060a57f8ebc71aba88fb7a168efe28a88186a
aviralnimbekar <aviralnimbekar12@gmail.com>
2021-08-08 00:33:18 +0530

    Adding Part time Employee and Wage

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -3,20 +3,25 @@ public class EmployeeWage {
     public static void main(String[] args) {
         System.out.println("Welcome to Employee Wage Computation Program");
 
-        int is_Present = 1;
+        int is_Full_Time_Present = 1;
+        int is_Part_Time_Present = 2;
         int emp_Wage_Per_hrs = 20;
         int emp_Working_Hrs = 0;
-        double empCheck = Math.floor(Math.random() * 10) % 2;
+        double empCheck = Math.floor(Math.random() * 10) % 3;
 
-        if ( empCheck == is_Present ) {
-            System.out.println("Employee is Present");
+        if ( empCheck == is_Full_Time_Present ) {
+            System.out.println("Employee is Full Time Present");
             emp_Working_Hrs = 8;
+        }
+        else if ( empCheck == is_Part_Time_Present ) {
+            System.out.println("Employee is Part Time Present");
+            emp_Working_Hrs = 4;
         }
         else {
             System.out.println("Employee is Absent");
         }
 
         int Salary = emp_Wage_Per_hrs * emp_Working_Hrs;
-        System.out.println(Salary);
+        System.out.println("Salary = " + Salary);
     }
 }
