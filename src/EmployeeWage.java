fc70fe7e767f640e45923985f87dc394f81b5721
aviralnimbekar <aviralnimbekar12@gmail.com>
2021-08-07 23:52:41 +0530

    Computation of daily wage of Employee

--- src/EmployeeWage.java
+++ src/EmployeeWage.java
@@ -4,10 +4,19 @@ public class EmployeeWage {
         System.out.println("Welcome to Employee Wage Computation Program");
 
         int is_Present = 1;
+        int emp_Wage_Per_hrs = 20;
+        int emp_Working_Hrs = 0;
         double empCheck = Math.floor(Math.random() * 10) % 2;
-        if ( empCheck == is_Present )
+
+        if ( empCheck == is_Present ) {
             System.out.println("Employee is Present");
-        else
-        System.out.println("Employee is Absent");
+            emp_Working_Hrs = 8;
+        }
+        else {
+            System.out.println("Employee is Absent");
+        }
+
+        int Salary = emp_Wage_Per_hrs * emp_Working_Hrs;
+        System.out.println(Salary);
     }
 }
